d642d4730e58f7a539c73b288eb775aa845aa8b5
github username <github email address>
2018-01-31 18:49:29 -0500

    added tests

---
+++ tests/acceptance/board-test.js
@@ -0,0 +1,42 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'trello/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | board')
+
+test('should show lists as the home page', function (assert) {
+})
+
+test('should link to add a list', function (assert) {
+})
+
+test('should not be able to add an empty string as a list', function (assert) {
+})
+
+test('should link to add a task', function (assert) {
+})
+
+test('should not be able to add an empty string as a task', function (assert) {
+})
+
+test('should be able to edit a task', function (assert) {
+})
+
+test('should be able to edit a list name', function (assert) {
+})
+
+test('should be able to see details of a task', function (assert) {
+})
+
+test('should be able to delete a list', function (assert) {
+})
+
+test('should be able to delete a task', function (assert) {
+})
+
+
+/*
+Should pop out an editing modal onClick
+Should be able to close the modal
+Should be able to delete a list from the Modal
+Clicking “delete” should display a confirmation alert before deleting
+*/
