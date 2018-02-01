3ade42d4e81466487ee96ecec4f8648d63e0ac5a
github username <github email address>
2018-01-31 19:00:02 -0500

    defined lists

---
+++ app/routes/index.js
@@ -0,0 +1,19 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+	model() {
+    return [{
+      id: '1',
+      title: 'To Do',
+      description: 'Things I hope I dont forget to do'
+    }, {
+      id: '2',
+      title: 'Grocery List',
+      description: 'Menu Planning for the week'
+    }, {
+      id: '3',
+      title: 'Projects',
+      description: 'things i mean to work on'
+    }];
+  }
+});
