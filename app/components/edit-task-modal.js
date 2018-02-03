26fac822ac53d75a071f621488d9f367d780e98c
github username <github email address>
2018-02-02 22:29:34 -0500

    other ways of using modals

--- app/components/edit-task-modal.js
+++ app/components/edit-task-modal.js
@@ -2,30 +2,7 @@ import Component from '@ember/component';
 // import layout from '../templates/components/edit-task-modal';
 
 export default Ember.Component.extend({
-  expose: function() {
-    var app_controller = this.get('targetObject');
-    var exposedName = "comp-" + this.get('id');
-    // {{debugger}}
-    app_controller.set(exposedName, this);
-	}.on('init'),
+
 	
-	actions: {
-    toggleModal: function() {
-        this.toggleProperty('enabled')
-    },
-   //  addDescription: function() {
-   //  	let taskDescription = this.get('description')
-   //  	let taskId = this.get('task.id')
-			// //console.log()
-			
-			// Task.find(taskId).setProperties({ description:taskDescription })			
-			// //this.get('list.tasks').pushObject(newTask)
 
-    // },
-    deleteTask: function() {
-    	var taskId = this.get(target) 
-    	console.log("i am in delete task", taskId)
-    }
-
-	}
 });
--- app/components/task-display.js
+++ app/components/task-display.js
@@ -1,27 +1,13 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-	// isShowingModal: false,
-	actions: {
-		// editTaskName: function() {
-		// 	let taskName = this.get('taskName')
-		// 	if ( !taskName || !taskName.trim() ) {
-		// 			return false
-		// 		}
-		// 	//console.log()
-		// 	var newTask = Task.create().setProperties({taskName: taskName, description:'' })			
-		// 	this.get('list.tasks').pushObject(newTask)
-		// },
-		// editTaskDescription: function(){
-		// 	let description = this.get('description')
-		// },
-   openModal: function(target) {
-      var modal = this.get('comp-' + target);
-      modal.send('toggleModal')
+	isShowingModal: false,
+  actions: {
+    toggleModal: function() {
+      this.toggleProperty('isShowingModal');
     }
-		
-	}
-});
+  }
+})
 
 
 
