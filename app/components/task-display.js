e562e4378e0d792e0dc3c1c6591505234344dd4a
github username <github email address>
2018-02-04 14:54:05 -0500

    resolve merge conflicts in task-display

--- app/components/task-display.js
+++ app/components/task-display.js
@@ -1,14 +1,6 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-<<<<<<< HEAD
-	actions: {
-
-   openModal: function(target) {
-   	//target is ID
-      var modal = this.get('comp-' + target);
-      modal.send('toggleModal')
-=======
 	isShowingModal: false,
   actions: {
     toggleModal: function() {
