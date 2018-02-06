5962d89bd4503606b302eab8aac74f6fb8025be3
github username <github email address>
2018-02-06 12:58:33 -0500

    changed default display

--- app/routes/lists.js
+++ app/routes/lists.js
@@ -6,23 +6,23 @@ export default Route.extend({
     return [
       {
         name: 'Default List',
-        tasks: [{id:0, taskName:'default task', description:'words'}]     
+        tasks: [/*{id:0, taskName:'default task', description:'words'}*/]     
       }/*,
     {
         name: 'To Do',
         tasks: [
           {id:1, taskName:'get buttons to work', description:'today'}, 
-          {id: 2, taskName:'wash dishes', description:'also must happen today'}]     
+          {id: 2, taskName:'wash dishes', description:'also should happen today'}]     
       }, {
         name: 'Projects',
         tasks: [
-          {id: 3, taskName:'ember', description:'wahhhhh'}, 
+          {id: 3, taskName:'ember', description:'learn testing'}, 
           {id: 5, taskName:'snake name change', description:'probably wont get to this'} 
         ]        
       }, {
         name: 'Groceries',
         tasks: [{id: 5, taskName:'apples', description:''}, 
-                {id: 6, taskName:'kale', description:''}]         
+                {id: 6, taskName:'kale', description:'Brooklyn'}]         
       }*/
     ]
   }
